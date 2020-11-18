9cf8c34b97f9acf1fe2f90f0f3c8b5093cc8121d
shihab-sadik <60620200+shihab-sadik@users.noreply.github.com>
2020-11-18 15:37:52 +0600

    Add files via upload

---
+++ LT-05/controller/updateProduct.php
@@ -0,0 +1,22 @@
+<?php  
+require_once '../model.php';
+
+
+if (isset($_POST['updateProduct'])) {
+	$data['name'] = $_POST['name'];
+	$data['buyingprice'] = $_POST['buyingprice'];
+	$data['sellingprice'] = $_POST['sellingprice'];
+	
+
+  if (updateProduct($_POST['id'], $data)) {
+  	echo 'Successfully updated!!';
+  	//redirect to showProduct
+  	header('Location: ../showProduct.php?id=' . $_POST["id"]);
+  }
+}	 
+else {
+	echo 'You are not allowed to access this page.';
+}
+
+
+?>
