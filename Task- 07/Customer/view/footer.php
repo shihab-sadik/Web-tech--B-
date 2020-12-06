5ed6afd53ec9839212c74bbc9c982b79cbc0a0ee
shihab-sadik <60620200+shihab-sadik@users.noreply.github.com>
2020-12-06 21:41:13 +0600

    Add files via upload

---
+++ Task- 07/Customer/view/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Footer</title>
+</head>
+<body>
+	<h4>Contact Us</h4>
+	<b>Email:</b>atrms@outlook.com <br>
+	<b>Phone:</b>+99605534 <br>
+	<b>Address:</b>Nikunja-2,Dhaka <br>
+
+
+</body>
+</html>
